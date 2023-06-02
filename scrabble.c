1f6a1281953adc83d05dde4e695e35e5955c62a1
Imen <imen.benrhouma93@gmail.com>
2023-06-02 11:56:27 +0200

    Lab2 cs50x

---
+++ scrabble.c
@@ -0,0 +1,63 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word, int ARRAY[]);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1, POINTS);
+    int score2 = compute_score(word2, POINTS);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+
+    return 0;
+}
+
+int compute_score(string word, int ARRAY[])
+{
+    int s = strlen(word);
+    int lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int score = 0;
+//comparing lettre from the list % lettre (w) of the word)
+    for (int j = 0; j < s; j++)
+    {
+        char w = word[j];
+
+        if (isalpha(w))
+        {
+            for (int i = 0 ; i < 26 ; i++)
+            {
+                if (upper[i] == w || lower[i] == w)
+                {
+                    score = score + POINTS[i];
+                }
+            }
+
+        }
+
+    }
+    return score;
+}
